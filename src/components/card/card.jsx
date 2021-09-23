a40a0eb0b228f710941e80393de56e0705d96fa8
Cara94 <83941852+Cara94@users.noreply.github.com>
2021-09-23 16:48:12 +0300

    update from monsters to kitties

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,6 +4,7 @@ export const Card = (props) => (
     <div className="card-container">
         <img src={`https://robohash.org/${props.monster.id}?set=set4&size=180x180`} alt="monster" />
         <h2> { props.monster.name } </h2>
+        <h3> { props.monster.username } </h3>
         <p> { props.monster.email } </p>
     </div>
 )
